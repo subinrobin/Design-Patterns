793fefe78e62aae1971706ff4cbbf3c7b9d9682e
subin <subinathome@gmail.com>
2023-12-18 14:15:40 +0530

    add condiment cost

--- src/main/scala/patterns/oops/decoratorPattern/condiment/CondimentDecorator.scala
+++ src/main/scala/patterns/oops/decoratorPattern/condiment/CondimentDecorator.scala
@@ -1,8 +1,11 @@
 package patterns.oops.decoratorPattern.condiment
 
 import patterns.oops.decoratorPattern.beverage.Beverage
+import patterns.oops.decoratorPattern.beverage.sizes.BeverageSize
 
 abstract class CondimentDecorator(beverage: Beverage) extends Beverage {
 
     def description: String
+
+    override def size: BeverageSize = beverage.size
 }
--- src/main/scala/patterns/oops/decoratorPattern/condiment/Soy.scala
+++ src/main/scala/patterns/oops/decoratorPattern/condiment/Soy.scala
@@ -1,11 +1,20 @@
 package patterns.oops.decoratorPattern.condiment
 
 import patterns.oops.decoratorPattern.beverage.Beverage
+import patterns.oops.decoratorPattern.beverage.sizes.TALL
+import patterns.oops.decoratorPattern.beverage.sizes.GRANDE
+import patterns.oops.decoratorPattern.beverage.sizes.VENTI
 
 class Soy(beverage: Beverage) extends CondimentDecorator(beverage) {
-    
+
     override def description: String = s"${beverage.description}, Soy"
 
-    def cost(): Double = beverage.cost() + .15
-
+    def cost(): Double = {
+        val soyCost = beverage.size match {
+            case TALL   => .10
+            case GRANDE => .15
+            case VENTI  => .20
+        }
+        beverage.cost() + soyCost
+    }
 }
